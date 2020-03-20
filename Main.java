673af08eb4059198d1217ea7991037a44e91c308
mahikarao <62445290+mahikarao@users.noreply.github.com>
2020-03-21 03:02:34 +0530

    Comments Added

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ public class Main
             int_tokens[i]=Integer.parseInt(tokens[i]);}
         return int_tokens;
     }
-    
     //perform comparison function on stock prices-- right operating array;
     static int lucky_days(int [] int_tokens){
         int num_days=int_tokens.length;
@@ -17,7 +16,7 @@ public class Main
         for(int m=0;m<num_days;m++){
             int max=int_tokens[m];
             boolean isLucky=true;
-            
+		
             for(int n=m+1;n<num_days;n++){
                 if(int_tokens[n]>max){
                     isLucky=false;
@@ -37,11 +36,12 @@ public class Main
     
 	public static void main(String[] args) {
 		int T=sc.nextInt();
-		int [] test_res=new int[T]; //An array to store the result of each test case, all at once;
+		int [] test_res=new int[T]; 
+		//An array to store the result of each test case, all at once;
 		
 		for(int j=0;j<T;j++){
 		    int days=sc.nextInt();
-		    sc.nextLine();
+		    sc.nextLine(); //nextInt cant implicitly process nextLine command.This prevents compiler from skipping line 45;
 		    String input=sc.nextLine();
 		    
 		    //calling the two defined functions;
@@ -49,7 +49,7 @@ public class Main
 		    test_res[j]=lucky_days(int_tokens);
 		    
 		}
-		
+		//Used to print the final result of all given test cases at once;
 		for(int num_lucky : test_res){
 		    System.out.println(num_lucky);
 		}
